eee3ed41debb2623e0b55407a227fe447b420875
L-ING <hlf01@icloud.com>
2023-11-26 16:02:45 +0800

    完成block.py中文注释

--- block.py
+++ block.py
@@ -2,12 +2,17 @@ __author__ = 'alexisgallepe'
 
 from enum import Enum
 
+# 片段中每个块的大小
 BLOCK_SIZE = 2 ** 14
 
 
+# 块的状态
 class State(Enum):
+    # 未开始下载
     FREE = 0
+    # 正在下载
     PENDING = 1
+    # 已下载完成
     FULL = 2
 
 
@@ -15,7 +20,9 @@ class Block():
     def __init__(self, state: State = State.FREE, block_size: int = BLOCK_SIZE, data: bytes = b'', last_seen: float = 0):
         self.state: State = state
         self.block_size: int = block_size
+        # 块的真实数据
         self.data: bytes = data
+        # 记录最后一次见到该数据块的时间戳，用于确定一个正在下载但长时间未更新的数据块是否应该重新标记为FREE
         self.last_seen: float = last_seen
 
     def __str__(self):
